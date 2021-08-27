700020f46434b81f2edb666204b341575f2e080a
CesarRomeroDev <cesar_romero.dev@outlook.com>
2021-08-27 16:30:24 -0500

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-//import { ContadorComponent } from './contador/contador/contador.component';
+import { ContadorComponent } from './contador/contador/contador.component';
 import { HeroesModule } from './heroes/heroes.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    //ContadorComponent,
+    ContadorComponent,
   ],
   imports: [
     BrowserModule,
